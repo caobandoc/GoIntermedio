01961d6988ff262b6921da3bf313a9afc3d390b0
caobandoc <obandocarlosandres@gmail.com>
2022-03-25 20:06:37 -0500

    Constructores

--- class.go
+++ class.go
@@ -3,8 +3,17 @@ package main
 import "fmt"
 
 type Employee struct {
-	id   int
-	name string
+	id       int
+	name     string
+	vacation bool
+}
+
+func NewEmployee(id int, name string, vacation bool) *Employee {
+	return &Employee{
+		id:       id,
+		name:     name,
+		vacation: vacation,
+	}
 }
 
 func (e *Employee) SetId(id int) {
@@ -24,14 +33,36 @@ func (e *Employee) GetName() string {
 }
 
 func main() {
+	// Constructores
+	// 1
 	e := Employee{}
+	fmt.Printf("%v\n", e)
+
+	// 2
+	e2 := Employee{
+		id:       2,
+		name:     "John",
+		vacation: true,
+	}
+	fmt.Printf("%v\n", e2)
+
+	// 3
+	e3 := new(Employee)
+	fmt.Printf("%v\n", *e3)
+	e3.id = 3
+	e3.name = "Name"
+	fmt.Printf("%v\n", *e3)
+
+	// 4
+	e4 := NewEmployee(4, "Name", true)
+	fmt.Printf("%v\n", *e4)
+
+	//e.id = 1
+	//e.name = "Carlos"
 	//fmt.Printf("%v", e)
-	e.id = 1
-	e.name = "Carlos"
+	//e.SetId(5)
+	//e.SetName("Andres")
 	//fmt.Printf("%v", e)
-	e.SetId(5)
-	e.SetName("Andres")
-	//fmt.Printf("%v", e)
-	fmt.Println(e.GetId())
-	fmt.Println(e.GetName())
+	//fmt.Println(e.GetId())
+	//fmt.Println(e.GetName())
 }
